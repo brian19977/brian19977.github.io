467755ee450fc0c899304d16a3fbf9e6698197ee
Brian Paick <brian19977@github.com>
2022-05-28 23:39:12 -0600

    case sensitive filename

--- writing/writing_homepage.js
+++ writing/writing_homepage.js
@@ -1,7 +1,7 @@
 var articles = [
     {
         title: "Concert Review: Marc-André Hamelin, 15 November 2018",
-        link: "reviews/181115-Hamelin.html",
+        link: "reviews/181115-hamelin.html",
         image: "181115-Hamelin.jpg",
         description: "Marc-André Hamelin, the Canadian virtuoso composer-pianist ..."+
         " delivered a surpassingly excellent performance.",
