87adfa761fe906e0011bf9fe50b53539b6d68f64
brian19977 <67242750+brian19977@users.noreply.github.com>
2023-03-19 21:44:52 -0400

    updates page not ready yet

--- main.js
+++ main.js
@@ -14,3 +14,40 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+/*
+var updates = [
+    {
+        title: "Moving to DC",
+        link: "",
+        image: "profile.jpeg"
+    },
+    {
+        title: "Graduating from Columbia",
+        link: "",
+        image: "profile.jpeg"
+    },
+    {
+        title: "Piano Recital IV: Graduation Recital, 8 May 2022",
+        link: "",
+        image: "profile.jpeg"
+    }
+];
+
+var host_div = document.getElementById("update-cards");
+
+updates.forEach(element => {
+    var card_div = document.createElement("div");
+    card_div.classList.add("card", "col-sm-6", "col-12", "border-0");
+
+    var card_image = document.createElement("img");
+    card_image.src = element.image;
+    card_image.style.cssText = "object-fit: cover; max-height: 100%; max-width: 100%;";
+
+    var card_text = document.createElement("p");
+    card_text.innerHTML = element.title;
+
+    card_div.appendChild(card_image);
+    card_div.appendChild(card_text);
+    host_div.appendChild(card_div);
+});
+*/
