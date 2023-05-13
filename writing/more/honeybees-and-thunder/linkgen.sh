c26a1d0f9982c77047d4a13981c2d984945cf849
brian19977 <67242750+brian19977@users.noreply.github.com>
2023-05-13 19:02:40 -0400

    site updated

--- writing/more/honeybees-and-thunder/linkgen.sh
+++ writing/more/honeybees-and-thunder/linkgen.sh
@@ -1,5 +1,22 @@
-linkpt1="<a href='honeybees-and-thunder/1-preliminaries/"
+# Check if directory name is provided
+if [ -z "$1" ]; then
+  echo "Please provide a directory name as a command-line argument."
+  exit 1
+fi
+
+# Check if the directory exists
+if [ ! -d "$1" ]; then
+  echo "Directory does not exist."
+  exit 1
+fi
+
+linkpt1="<a href='honeybees-and-thunder/"
 linkpt2="'><p></p></a>"
-for f in *; do
-    echo "$linkpt1$f$linkpt2"
+
+# Loop through the files in the directory
+for file in "$1"/*; do
+  if [ -f "$file" ]; then
+    echo "$linkpt1$file$linkpt2"
+  fi
 done
+
