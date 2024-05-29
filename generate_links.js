316fff60dab9d70b56242f3c38d9a7a3cf93ee13
brian19977 <brianpaick@gmail.com>
2024-05-29 14:40:05 -0400

    small edits in honeybee, reverse order in reviews

--- generate_links.js
+++ generate_links.js
@@ -1,4 +1,4 @@
-function generate_links(category) {
+function generate_links(category, oldestToNewest = true) {
     var sections = document.getElementsByClassName("content");
     var id_arr = [];
     var i = 0;
@@ -10,24 +10,34 @@ function generate_links(category) {
     var section_arr = Array(id_arr.length);
     for(; i < id_arr.length; i++)
         section_arr[i] = document.getElementById(id_arr[i]);
-    i = 0;
-    
-    for(; i < articles.length; i++) {
-        var tag_set = new Set(articles[i].tags);
 
-        for(var j = 0; j < section_arr.length; j++) {
-            if(
-                    (tag_set.has(id_arr[j])) && 
-                    (category.length > 0 ? tag_set.has(category) : true)) {
-
-                var link = document.createElement("a");
-                link.href = articles[i].link.substring(articles[i].link.indexOf("/")+1);
-                var text = document.createElement("p");
-                text.innerHTML = articles[i].title;
-                link.appendChild(text);
-                section_arr[j].appendChild(link);
-            }
+    if(oldestToNewest) {
+        i = 0;
+        for(; i < articles.length; i++) {
+            create_links(category, section_arr, id_arr, i);
+        }
+    } else {
+        i = articles.length - 1;
+        for(; i > -1; i--) {
+            create_links(category, section_arr, id_arr, i);
         }
     }
+}
+
+function create_links(category, section_arr, id_arr, i) {
+    var tag_set = new Set(articles[i].tags);
     
+    for(var j = 0; j < section_arr.length; j++) {
+        if(
+                (tag_set.has(id_arr[j])) && 
+                (category.length > 0 ? tag_set.has(category) : true)) {
+
+            var link = document.createElement("a");
+            link.href = articles[i].link.substring(articles[i].link.indexOf("/")+1);
+            var text = document.createElement("p");
+            text.innerHTML = articles[i].title;
+            link.appendChild(text);
+            section_arr[j].appendChild(link);
+        }
+    }
 }
